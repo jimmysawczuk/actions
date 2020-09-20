80de60a7ca98b9476455e2d43d147ed12bb72d6c
Jimmy Sawczuk <me@jimmysawczuk.com>
2020-09-19 20:00:20 -0400

    First version of job started

--- telegram/job-started/src/main.js
+++ telegram/job-started/src/main.js
@@ -1,5 +1,41 @@
+const core = require("@actions/core")
+const querystring = require("querystring")
+const fetch = require("node-fetch")
+const { default: sendMessage } = require("../../../lib/telegram")
+
+function getMessage(commit, repo, job, runID, workflow) {
+  const runURL = `https://github.com/${repo}/runs/${runID}`
+  const commitURL = `https://github.com/${repo}/commit/${commit}`
+  const shortCommit = commit.substr(0, 7)
+
+  return `<b>Job ${job} <a href="${runURL}">started</a>:</b>
+
+ - commit: <a href="${commitURL}">${shortCommit}</a>`
+}
+
 async function run() {
-  console.log("gothere")
+  try {
+    const chatID = core.getInput("chatID", { required: true })
+    const telegramBotURL = core.getInput("telegramBotURL", {
+      required: true,
+    })
+    const commit = process.env.GITHUB_SHA
+    const repo = process.env.GITHUB_REPOSITORY
+    const job = process.env.GITHUB_JOB
+    const runID = process.env.GITHUB_RUN_ID
+    const workflow = process.env.GITHUB_WORKFLOW
+
+    sendMessage(
+      telegramBotURL,
+      chatID,
+      getMessage(commit, repo, job, runID, workflow),
+      {
+        parseMode,
+      },
+    )
+  } catch (e) {
+    core.setFailed(e)
+  }
 }
 
 run()
