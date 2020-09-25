7d641db2a9711b1145b46edcdfd68c1f56c6c44e
Jimmy Sawczuk <me@jimmysawczuk.com>
2020-09-25 07:41:05 -0400

    Fleshing out failed

--- telegram/job-failed/src/main.js
+++ telegram/job-failed/src/main.js
@@ -1,14 +1,15 @@
 const core = require("@actions/core")
 const { default: sendMessage } = require("../../../lib/telegram")
 
-function getMessage(commit, repo, job, runID, workflow) {
+function getMessage(commit, repo, job, runID, workflow, error) {
   const runURL = `https://github.com/${repo}/runs/${runID}`
   const commitURL = `https://github.com/${repo}/commit/${commit}`
   const shortCommit = commit.substr(0, 7)
 
   return `<b>Job ${job} <a href="${runURL}">failed</a>:</b>
 
- - commit: <a href="${commitURL}">${shortCommit}</a>`
+ - commit: <a href="${commitURL}">${shortCommit}</a>
+ - error: ${error}`
 }
 
 async function run() {
@@ -17,18 +18,21 @@ async function run() {
     const telegramBotURL = core.getInput("telegramBotURL", {
       required: true,
     })
+    const error = core.getInput("error")
     const commit = process.env.GITHUB_SHA
     const repo = process.env.GITHUB_REPOSITORY
     const job = process.env.GITHUB_JOB
     const runID = process.env.GITHUB_RUN_ID
     const workflow = process.env.GITHUB_WORKFLOW
 
+    console.log(process.env)
+
     sendMessage(
       telegramBotURL,
       chatID,
-      getMessage(commit, repo, job, runID, workflow),
+      getMessage(commit, repo, job, runID, workflow, error),
       {
-        parseMode,
+        parseMode: "HTML",
       },
     )
   } catch (e) {
