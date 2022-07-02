e8e59dfc88b69dadefd195d872671e4206704797
Jimmy Sawczuk <me@jimmysawczuk.com>
2022-07-02 12:00:35 -0400

    get-tag-from-ref pulls branch as well

--- github/get-tag-from-ref/src/main.js
+++ github/get-tag-from-ref/src/main.js
@@ -2,9 +2,17 @@ const core = require("@actions/core")
 
 async function run() {
   try {
-    const tagName = core.getInput("ref", { required: true })
-    const tag = tagName.replace("refs/tags/", "")
-    core.setOutput("tag", tag)
+    const ref = core.getInput("ref", { required: true })
+
+    if (ref.match(new RegExp("^refs/tags/"))) {
+      const tag = ref.replace("refs/tags/", "")
+      core.setOutput("tag", tag)
+
+      if (ref.match(new Regexp("^ref/heads/"))) {
+        const branch = ref.replace("refs/heads/", "")
+        core.setOutput("branch", branch)
+      }
+    }
   } catch (e) {
     core.setFailed(e.message)
   }
