9b06ea70044b5b83e6949542b94d015206306368
Jimmy Sawczuk <me@jimmysawczuk.com>
2026-01-25 16:39:20 -0500

    Deprecate some actions

--- util/dirs.js
+++ util/dirs.js
@@ -1,9 +1,1 @@
-module.exports = [
-  "github/get-tag-from-ref",
-  "github/upload-release-asset",
-  "telegram/job-failed",
-  "telegram/job-started",
-  "telegram/job-succeeded",
-  "telegram/send-message",
-  "telegram/workflow-started",
-]
+module.exports = ["github/get-tag-from-ref"]
